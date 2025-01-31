e2ac8ec62b60765991da6ffe8237075b2b966528
amitksingh1490 <amitksingh1490@users.noreply.github.com>
2025-01-31 14:10:00 -0800

    Update SHA256 checksum for ARM architecture in Codeforge formula

--- Formula/codeforge.rb
+++ Formula/codeforge.rb
@@ -9,7 +9,7 @@ class Codeforge < Formula
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   elsif Hardware::CPU.arm?
     url "https://release-download.tailcall.workers.dev/download/v0.1.0/forge-aarch64-apple-darwin"
-    sha256 "308e51b36d0b385710e21eda48cc1b88308ed77d209fe48048118dcc638f2aef"
+    sha256 "dae6af971acdd16e68350799cce0ce7e325e0c8cbfe47df00c3978b005e6c70a"
   end
 
   def install
